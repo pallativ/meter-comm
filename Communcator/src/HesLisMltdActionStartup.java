3f7e205bf1ba008351a1a966bc47f9fa7c69dd61
Veera Pallati <veera@coralInnovations.in>
2023-01-06 06:34:55 +0530

    Refactoring the code to better understand

--- Communcator/src/HesLisMltdActionStartup.java
+++ Communcator/src/HesLisMltdActionStartup.java
@@ -1,3 +1,4 @@
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -5,151 +6,148 @@ import java.util.Properties;
 
 import org.apache.log4j.Logger;
 
-import com.hes.lis.mltd.process.HesLisMltdProcessor;
+import com.hes.lis.mltd.process.MeterComService;
 import com.hes.lis.mltd.utils.Utils;
 
 /**
- * 
+ *
  * @author psvr
  *
  */
 public class HesLisMltdActionStartup {
-	
-	private static final Logger logger=Logger.getLogger(TcpModemIpListenerServer.class);
-	
-	private static ServerSocket serverSocketObj;
-	private static boolean isServerStarted = false;
-	private static Properties prop;
-	private static Socket socketObj;
 
-	public void tcpServerStart(Integer portNumber) {
-		System.out.println("HesLisMltdActionStartup Server Trying to Start on Port :: "+portNumber);
-		try {
-			
-			serverSocketObj = new ServerSocket(portNumber);
-			if (serverSocketObj != null) {
-				isServerStarted = true;
-				System.out.println("HesLisMltdActionStartup Server Started Successfully on Port :: "+portNumber);
-			}
-		} catch (IOException e) {
-			System.out.println("Error in Starting HesLisMltdActionStartup Listener on Port :: "+portNumber+" :: "+e.getMessage());
-			e.printStackTrace();
-		}
-	}
+    private static final Logger logger = Logger.getLogger(TcpModemIpListenerServer.class);
 
-	public void invokeProcess(Integer portNumber) {
+    private static ServerSocket serverSocket;
+    private static boolean isServerStarted = false;
+    private static Properties prop;
+    private static Socket socket;
 
-		try {
-			while(true){
-				
-				tcpServerStart(portNumber);
-				socketObj=null;
-				try{
-					while (isServerStarted) {
-						try{
-							socketObj = serverSocketObj.accept();
-							boolean canStartUpplication=true;
-							if(canStartUpplication){
-								TcpModemIpListenerServer threadObj = new TcpModemIpListenerServer(
-										socketObj,prop);
-								threadObj.start();
-							}else{
-								System.out.println("******************************************************************************");
-								System.out.println("HES-LIS-MLTD Liscense Expired.Cannot Run Application.Please contact Admin team");
-								System.out.println("******************************************************************************");
-							}
-							
-							
-						}catch (Exception e) {
-							System.out.println("while (isServerStarted) { :: "+e.getMessage());
-							e.printStackTrace();
-						}
-						
-						if(serverSocketObj==null){
-							logger.error("serverSocketObj==null");
-							isServerStarted=false;
-						}
-					}
-				}catch (Exception e) {
-					System.out.println("Error in processStart :: "+e.getMessage());
-					e.printStackTrace();
-				}finally{
+    public void tcpServerStart(Integer portNumber) {
+        System.out.println("HesLisMltdActionStartup Server Trying to Start on Port :: " + portNumber);
+        try {
+
+            serverSocket = new ServerSocket(portNumber);
+            if (serverSocket != null) {
+                isServerStarted = true;
+                System.out.println("HesLisMltdActionStartup Server Started Successfully on Port :: " + portNumber);
+            }
+        } catch (IOException e) {
+            System.out.println("Error in Starting HesLisMltdActionStartup Listener on Port :: " + portNumber + " :: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void startListeningOnPort(Integer portNumber) {
+
+        try {
+            while (true) {
+
+                tcpServerStart(portNumber);
+                socket = null;
+                try {
+                    while (isServerStarted) {
+                        try {
+                            socket = serverSocket.accept();
+                            boolean canStartUpplication = true;
+                            if (canStartUpplication) {
+                                TcpModemIpListenerServer threadObj = new TcpModemIpListenerServer(
+                                        socket, prop);
+                                threadObj.start();
+                            } else {
+                                System.out.println("******************************************************************************");
+                                System.out.println("HES-LIS-MLTD Liscense Expired.Cannot Run Application.Please contact Admin team");
+                                System.out.println("******************************************************************************");
+                            }
+
+                        } catch (Exception e) {
+                            System.out.println("while (isServerStarted) { :: " + e.getMessage());
+                            e.printStackTrace();
+                        }
+
+                        if (serverSocket == null) {
+                            logger.error("serverSocketObj==null");
+                            isServerStarted = false;
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println("Error in processStart :: " + e.getMessage());
+                    e.printStackTrace();
+                } finally {
 //					try {
-//						if (socketObj != null) {
-//							socketObj.close();
+//						if (socket != null) {
+//							socket.close();
 //						}
 //					} catch (Exception e) {
 //						e.printStackTrace();
 //						logger.error("Error in Closing Socket Connection :: "+e.getMessage());
 //					}
-				}
+                }
 //				Thread.sleep(1000);
-			}
-			
-		}catch (Exception e) {
-			System.out.println("Error in Closing Socket Connection :: "+e.getMessage());
-			e.printStackTrace();
-		}finally{
-			try {
-//				if (socketObj != null) {
-//					socketObj.close();
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error in Closing Socket Connection :: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            try {
+//				if (socket != null) {
+//					socket.close();
 //				}
-			} catch (Exception e) {
-				System.out.println("Error in Closing Socket Connection :: "+e.getMessage());
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	
-	private static class TcpModemIpListenerServer extends Thread {
-		private Socket socketObjext;
-		private Properties propObj;
+            } catch (Exception e) {
+                System.out.println("Error in Closing Socket Connection :: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
 
-		public TcpModemIpListenerServer(Socket socketObj,Properties prop) {
-			this.socketObjext = socketObj;
-			this.propObj=prop;
-		}
+    private static class TcpModemIpListenerServer extends Thread {
 
-		public synchronized void run() {
-			try{
-				new HesLisMltdProcessor().processCommunication(socketObjext,propObj);
-			}catch (Exception e) {
-				System.out.println("Error in synchronized void run :: "+e.getMessage());
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	
-	public static void main(String[] args) {
-		
-		try{
-			boolean canStartUpplication=true;
+        private final Socket socket;
+        private final Properties properties;
 
-			if(canStartUpplication){
-				try{
-					System.out.println("HES-LIS-MLTD SmartMeter HES Tool started ===>>>>>");
-					prop=new Utils().readPortForTcpListener();
-					String port = prop.getProperty("TcpListenerPortNumber");
-					
-					try{
-						port=args[0];
-					}catch (Exception e) {
-						// TODO: handle exception
-					}
-					Integer portNumber=Integer.parseInt(port);
-					new HesLisMltdActionStartup().invokeProcess(portNumber);
-				}catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-			
-		}catch (Exception e) {
-			System.out.println("Error in starting Application");
-			e.printStackTrace();
-		}	
-	}
+        public TcpModemIpListenerServer(Socket socketObj, Properties properties) {
+            this.socket = socketObj;
+            this.properties = properties;
+        }
+
+        @Override
+        public synchronized void run() {
+            try {
+                new MeterComService().process(socket, properties);
+            } catch (Exception e) {
+                System.out.println("Error in synchronized void run :: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            boolean canStartUpplication = true;
+
+            if (canStartUpplication) {
+                try {
+                    System.out.println("HES-LIS-MLTD SmartMeter HES Tool started ===>>>>>");
+                    prop = new Utils().readPortForTcpListener();
+                    String port = prop.getProperty("TcpListenerPortNumber");
+
+                    try {
+                        port = args[0];
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
+                    Integer portNumber = Integer.parseInt(port);
+                    new HesLisMltdActionStartup().startListeningOnPort(portNumber);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error in starting Application");
+            e.printStackTrace();
+        }
+    }
 }
-
-
--- Communcator/src/com/hes/lis/mltd/utils/Utils.java
+++ Communcator/src/com/hes/lis/mltd/utils/Utils.java
@@ -16,254 +16,243 @@ import com.hes.lis.mltd.vo.MeterLogger;
 import com.hes.lis.mltd.vo.MeterLoggerId;
 
 /**
- * 
+ *
  * @author psvr
- * 
+ *
  */
 public class Utils {
-	
-	public Properties readPortForTcpListener(){
-		Properties prop = null;
-		try {
-			String workingDir = System.getProperty("user.dir");
-			prop = new Properties();
-			File configFilePath=new File(workingDir,"config.properties");
-			if(!configFilePath.exists()){
-				configFilePath=new File("/smartmeter/config.properties");
-			}
-			
-			FileInputStream propFileInputStream = new FileInputStream(configFilePath.getAbsolutePath());
-			prop.load(propFileInputStream);
-			propFileInputStream.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return prop;
-	}
-	
-	
-	public Properties readProperties(String fileName){
-		Properties prop = null;
-		try {
-			String workingDir = System.getProperty("user.dir");
-			prop = new Properties();
-			File configFilePath=new File(workingDir,fileName);
-			if(!configFilePath.exists()){
-				configFilePath=new File("/smartmeter/"+fileName);
-			}
-			
-			FileInputStream propFileInputStream = new FileInputStream(configFilePath.getAbsolutePath());
-			prop.load(propFileInputStream);
-			propFileInputStream.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return prop;
-	}
-	
-	
-	public Integer readSleepSeconds(){
-		Integer sleepSeconds=null;
 
-		Properties prop = new Properties();
-		try {
-			String workingDir = System.getProperty("user.dir");
-			prop = new Properties();
-			File configFilePath=new File(workingDir,"config.properties");
-			if(!configFilePath.exists()){
-				configFilePath=new File("/smartmeter/config.properties");
-			}
-			
-			FileInputStream propFileInputStream = new FileInputStream(configFilePath.getAbsolutePath());
-			prop.load(propFileInputStream);
-			propFileInputStream.close();
-			String sleepTimeInSeconds = prop.getProperty("sleepInSeconds");
-			sleepSeconds=Integer.parseInt(sleepTimeInSeconds);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return sleepSeconds;
-	}
-	
-	public Date convertEnglishToNepali(Date indianDate){
-		Date nepaliDate=indianDate;
-		try{
-			SimpleDateFormat dateTimeFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm");
-			SimpleDateFormat timeFormat=new SimpleDateFormat("HH:mm");		
-			
-    		NepaliDateUtils toDateConverter = new NepaliDateUtils();
-    		toDateConverter.setCurrentDate(indianDate);
-    		String toDateStr=toDateConverter.getNepaliYear()+"-"+(StringUtils.leftPad(toDateConverter.getNepaliMonth()+"", 2, "0"))+"-"+toDateConverter.getNepaliDate()+" "+timeFormat.format(indianDate);
-    		nepaliDate=dateTimeFormat.parse(toDateStr);
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-		return nepaliDate;
-	}
-	
-	
-	public Date convertNepaliToEnglish(Date nepaliDate){
-		Date istDate=nepaliDate;
-		try{
-			SimpleDateFormat dateTimeFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm");
-			SimpleDateFormat timeFormat=new SimpleDateFormat("HH:mm");		
-			
-    		NepaliDateUtils toDateConverter = new NepaliDateUtils();
-    		toDateConverter.setCurrentDate(nepaliDate);
-    		String toDateStr=toDateConverter.getNepaliYear()+"-"+(StringUtils.leftPad(toDateConverter.getNepaliMonth()+"", 2, "0"))+"-"+toDateConverter.getNepaliDate()+" "+timeFormat.format("");
-    		nepaliDate=dateTimeFormat.parse(toDateStr);
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-		return nepaliDate;
-	}
-	
-	
-	public static String dateTimeHexBytesByDate(Date date){
-		String strDate="";
-		Calendar currentDate = Calendar.getInstance();
-    	currentDate.setTime(date);
+    public Properties readPortForTcpListener() {
+        Properties prop = null;
+        try {
+            String workingDir = System.getProperty("user.dir");
+            prop = new Properties();
+            File configFilePath = new File(workingDir, "config.properties");
+            if (!configFilePath.exists()) {
+                configFilePath = new File("/smartmeter/config.properties");
+            }
 
-    	strDate=strDate+StringUtils.leftPad(String.format("%x", currentDate.get(Calendar.YEAR)), 4,"0")+" ";
-    	strDate=strDate+StringUtils.leftPad(String.format("%x", currentDate.get(Calendar.MONTH)+1), 2,"0")+" ";
-    	strDate=strDate+StringUtils.leftPad(String.format("%x", currentDate.get(Calendar.DAY_OF_MONTH)), 2,"0")+" ";
-    	strDate=strDate+"FF ";
-    	strDate=strDate+StringUtils.leftPad(String.format("%x", currentDate.get(Calendar.HOUR_OF_DAY)), 2,"0")+" ";
-    	strDate=strDate+StringUtils.leftPad(String.format("%x", currentDate.get(Calendar.MINUTE)), 2,"0")+" ";
-    	strDate=strDate+StringUtils.leftPad(String.format("%x", currentDate.get(Calendar.SECOND)), 2,"0")+" ";
-    	strDate=strDate+"FF 80 00 FF";
-		return " 09 0c "+strDate;
-	}
-	
-	public static String frameDataInTags(String tagName,List<String> dataList){
-		StringBuilder strb=null;
-		try{
-			if(dataList.size()>0){
-				strb=new StringBuilder();
-				strb.append("<"+tagName+">");
-				for(int i=0;i<dataList.size();i++){
-					String strElement=dataList.get(i);
+            FileInputStream propFileInputStream = new FileInputStream(configFilePath.getAbsolutePath());
+            prop.load(propFileInputStream);
+            propFileInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return prop;
+    }
+
+    public Properties readProperties(String fileName) {
+        Properties prop = null;
+        try {
+            String workingDir = System.getProperty("user.dir");
+            prop = new Properties();
+            File configFilePath = new File(workingDir, fileName);
+            if (!configFilePath.exists()) {
+                configFilePath = new File("/smartmeter/" + fileName);
+            }
+
+            FileInputStream propFileInputStream = new FileInputStream(configFilePath.getAbsolutePath());
+            prop.load(propFileInputStream);
+            propFileInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return prop;
+    }
+
+    public Integer readSleepSeconds() {
+        Integer sleepSeconds = null;
+
+        Properties prop = new Properties();
+        try {
+            String workingDir = System.getProperty("user.dir");
+            prop = new Properties();
+            File configFilePath = new File(workingDir, "config.properties");
+            if (!configFilePath.exists()) {
+                configFilePath = new File("/smartmeter/config.properties");
+            }
+
+            FileInputStream propFileInputStream = new FileInputStream(configFilePath.getAbsolutePath());
+            prop.load(propFileInputStream);
+            propFileInputStream.close();
+            String sleepTimeInSeconds = prop.getProperty("sleepInSeconds");
+            sleepSeconds = Integer.parseInt(sleepTimeInSeconds);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sleepSeconds;
+    }
+
+    public Date convertEnglishToNepali(Date indianDate) {
+        Date nepaliDate = indianDate;
+        try {
+            SimpleDateFormat dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+            SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+
+            NepaliDateUtils toDateConverter = new NepaliDateUtils();
+            toDateConverter.setCurrentDate(indianDate);
+            String toDateStr = toDateConverter.getNepaliYear() + "-" + (StringUtils.leftPad(toDateConverter.getNepaliMonth() + "", 2, "0")) + "-" + toDateConverter.getNepaliDate() + " " + timeFormat.format(indianDate);
+            nepaliDate = dateTimeFormat.parse(toDateStr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return nepaliDate;
+    }
+
+    public Date convertNepaliToEnglish(Date nepaliDate) {
+        Date istDate = nepaliDate;
+        try {
+            SimpleDateFormat dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+            SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+
+            NepaliDateUtils toDateConverter = new NepaliDateUtils();
+            toDateConverter.setCurrentDate(nepaliDate);
+            String toDateStr = toDateConverter.getNepaliYear() + "-" + (StringUtils.leftPad(toDateConverter.getNepaliMonth() + "", 2, "0")) + "-" + toDateConverter.getNepaliDate() + " " + timeFormat.format("");
+            nepaliDate = dateTimeFormat.parse(toDateStr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return nepaliDate;
+    }
+
+    public static String dateTimeHexBytesByDate(Date date) {
+        String strDate = "";
+        Calendar currentDate = Calendar.getInstance();
+        currentDate.setTime(date);
+
+        strDate = strDate + StringUtils.leftPad(String.format("%x", currentDate.get(Calendar.YEAR)), 4, "0") + " ";
+        strDate = strDate + StringUtils.leftPad(String.format("%x", currentDate.get(Calendar.MONTH) + 1), 2, "0") + " ";
+        strDate = strDate + StringUtils.leftPad(String.format("%x", currentDate.get(Calendar.DAY_OF_MONTH)), 2, "0") + " ";
+        strDate = strDate + "FF ";
+        strDate = strDate + StringUtils.leftPad(String.format("%x", currentDate.get(Calendar.HOUR_OF_DAY)), 2, "0") + " ";
+        strDate = strDate + StringUtils.leftPad(String.format("%x", currentDate.get(Calendar.MINUTE)), 2, "0") + " ";
+        strDate = strDate + StringUtils.leftPad(String.format("%x", currentDate.get(Calendar.SECOND)), 2, "0") + " ";
+        strDate = strDate + "FF 80 00 FF";
+        return " 09 0c " + strDate;
+    }
+
+    public static String frameDataInTags(String tagName, List<String> dataList) {
+        StringBuilder strb = null;
+        try {
+            if (dataList.size() > 0) {
+                strb = new StringBuilder();
+                strb.append("<" + tagName + ">");
+                for (int i = 0; i < dataList.size(); i++) {
+                    String strElement = dataList.get(i);
 //					strElement=strElement.replace(" ", "").toLowerCase();
-					
-					String dataString=strElement.toLowerCase()+"ff ff ";
-					String[] strArr=dataString.split(" ");
-					Integer sizeOfFrame=strArr.length-8;
-					String[] sizeArr=HexConvertionClassUtils.fetchSixeInHex(sizeOfFrame);
-					strArr[6]=sizeArr[0];
-					strArr[7]=sizeArr[1];
-					
-					for(String str:strArr){
-						strb.append(str);
-					}
-					
-					if(i!=dataList.size()-1){
-						strb.append("\n");
-					}
-					
-					
-					/*if(i!=dataList.size()-1){
+
+                    String dataString = strElement.toLowerCase() + "ff ff ";
+                    String[] strArr = dataString.split(" ");
+                    Integer sizeOfFrame = strArr.length - 8;
+                    String[] sizeArr = HexConvertionClassUtils.fetchSixeInHex(sizeOfFrame);
+                    strArr[6] = sizeArr[0];
+                    strArr[7] = sizeArr[1];
+
+                    for (String str : strArr) {
+                        strb.append(str);
+                    }
+
+                    if (i != dataList.size() - 1) {
+                        strb.append("\n");
+                    }
+
+                    /*if(i!=dataList.size()-1){
 						strb.append(strElement);
 					}else{
 						strb.append(strElement);
 					}*/
-				}
-				strb.append("</"+tagName+">");
-			}
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-		return strb.toString();
-	}
-	
-	
-	public static String frameDataInRAWFile(List<String> dataList){
-		StringBuilder strb=null;
-		try{
-			if(dataList.size()>0){
-				strb=new StringBuilder();
-				for(int i=0;i<dataList.size();i++){
-					String strElement=dataList.get(i);
-//					strElement=strElement.replace(" ", "").toLowerCase();
-					
-					String dataString=strElement.toLowerCase()+"ff ff ";
-					String[] strArr=dataString.split(" ");
-					Integer sizeOfFrame=strArr.length-8;
-					String[] sizeArr=HexConvertionClassUtils.fetchSixeInHex(sizeOfFrame);
-					strArr[6]=sizeArr[0];
-					strArr[7]=sizeArr[1];
-					
-					for(String str:strArr){
-						strb.append(str+" ");
-					}
-					
-					if(i!=dataList.size()-1){
-						strb.append("\n");
-					}
-				}
-			}
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-		return strb.toString();
-	}
-	
-	public static String frameDataStringInTags(String tagName,String dataStr){
-		StringBuilder strb=new StringBuilder();
-		try{
-			if(dataStr!=null&&dataStr.length()>0){
-				strb=new StringBuilder();
-				strb.append("<"+tagName+">");
-				
-				String dataString=dataStr.toLowerCase()+"ff ff ";
-				String[] strArr=dataString.split(" ");
-				Integer sizeOfFrame=strArr.length-8;
-				String[] sizeArr=HexConvertionClassUtils.fetchSixeInHex(sizeOfFrame);
-				strArr[6]=sizeArr[0];
-				strArr[7]=sizeArr[1];
-				
-				for(String str:strArr){
-					strb.append(str);
-				}
-				
-//				strb.append(dataStr.toLowerCase().replace(" ", ""));
-				strb.append("</"+tagName+">");
-			}
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-		return strb.toString();
-	}
-	
-	
-	
-	public static  void updateLoggerDB(String meterSerialNumber,String commPurpose,String commDetails){
-		
-		try{
-			
-			MeterLogger logger=new MeterLogger();
-			
-			MeterLoggerId meterLogId=new MeterLoggerId();
-			meterLogId.setCommDatetime(new Timestamp(new Date().getTime()));
-			meterLogId.setCommPurpose(commPurpose);
-			meterLogId.setMeterSerialNumber(meterSerialNumber);
-			logger.setId(meterLogId);
-			logger.setCommDetails(commDetails);
-			new MeterRuleEngineDAO().saveObject(logger);
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	public static void main(String[] arg){
-		try{
-			Utils.updateLoggerDB("123", "rwerewrwrwerwe", null);
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
+                }
+                strb.append("</" + tagName + ">");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return strb.toString();
+    }
 
+    public static String frameDataInRAWFile(List<String> dataList) {
+        StringBuilder strb = null;
+        try {
+            if (dataList.size() > 0) {
+                strb = new StringBuilder();
+                for (int i = 0; i < dataList.size(); i++) {
+                    String strElement = dataList.get(i);
+//					strElement=strElement.replace(" ", "").toLowerCase();
+
+                    String dataString = strElement.toLowerCase() + "ff ff ";
+                    String[] strArr = dataString.split(" ");
+                    Integer sizeOfFrame = strArr.length - 8;
+                    String[] sizeArr = HexConvertionClassUtils.fetchSixeInHex(sizeOfFrame);
+                    strArr[6] = sizeArr[0];
+                    strArr[7] = sizeArr[1];
+
+                    for (String str : strArr) {
+                        strb.append(str + " ");
+                    }
+
+                    if (i != dataList.size() - 1) {
+                        strb.append("\n");
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return strb.toString();
+    }
+
+    public static String frameDataStringInTags(String tagName, String dataStr) {
+        StringBuilder strb = new StringBuilder();
+        try {
+            if (dataStr != null && dataStr.length() > 0) {
+                strb = new StringBuilder();
+                strb.append("<" + tagName + ">");
+
+                String dataString = dataStr.toLowerCase() + "ff ff ";
+                String[] strArr = dataString.split(" ");
+                Integer sizeOfFrame = strArr.length - 8;
+                String[] sizeArr = HexConvertionClassUtils.fetchSixeInHex(sizeOfFrame);
+                strArr[6] = sizeArr[0];
+                strArr[7] = sizeArr[1];
+
+                for (String str : strArr) {
+                    strb.append(str);
+                }
+
+//				strb.append(dataStr.toLowerCase().replace(" ", ""));
+                strb.append("</" + tagName + ">");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return strb.toString();
+    }
+
+    public static void updateLoggerDB(String meterSerialNumber, String commPurpose, String commDetails) {
+
+        try {
+
+            MeterLogger logger = new MeterLogger();
+
+            MeterLoggerId meterLogId = new MeterLoggerId();
+            meterLogId.setCommDatetime(new Timestamp(new Date().getTime()));
+            meterLogId.setCommPurpose(commPurpose);
+            meterLogId.setMeterSerialNumber(meterSerialNumber);
+            logger.setId(meterLogId);
+            logger.setCommDetails(commDetails);
+            new MeterRuleEngineDAO().saveObject(logger);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] arg) {
+        try {
+            Utils.updateLoggerDB("123", "rwerewrwrwerwe", null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
